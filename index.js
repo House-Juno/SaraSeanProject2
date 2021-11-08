d8e3c5a1e294604d3389705cdfed5cc113186cc4
SaraMunir <sara.m07011@gmail.com>
2021-11-07 23:15:39 -0500

    added create quiz function

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-const app = {}
+const app = {};
+app.userScore = 0;
+app.questionArray = [];
+app.startButton = document.querySelector('#playButton');
+
+app.startButton.addEventListener('click', function(){
+    app.startButton.className='hidden'
+    app.getData()
+})
+
+app.getData = ()=>{
+    fetch(`https://game-of-thrones-quotes.herokuapp.com/v1/characters`)
+    .then(function(response){
+        return response.json();
+    })
+    .then((data)=>{
+        app.createQuiz(data);
+    })
+}
+app.createQuiz=(data)=>{
+    console.log('data: ', data)
+    
+}
+
 app.init = ()=>{
     console.log('welcome to our app!')
 }
